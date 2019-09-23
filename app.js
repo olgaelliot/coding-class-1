588cb0c013b2d9e44e6709c2423eb658ccd050be
oelliots <olgaschou@Olgas-MacBook-Pro.local>
2019-09-23 10:33:01 +0200

    sending request wiki

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-"name": "",
-"version": "0.0.1";
-"homepage": "";
-"description": "";
-"author": "Olga";
-"main": "app.js";
-"dependencies": {
-    "nodemon": "latest";
-    "express": "latest";
-    "request": "latest";
-    "cheerio": "latest";
-    var fs = require('fs');
+var express = require('express');
+var request = require('request');
+var fs = require('fs');
 
 //storing port number
+var port = 3000;
+var app = express();
+
+//setting up the boilerplate and routing
+app.get('/', function(req, res){
+
+var url = 'https://en.wikipedia.org/wiki/Osama_bin_Laden';
+
+//Making http request
+
+request(url), function(error, response, html) {
+  if(!error) {
+    res.send(html);
+  }
+});
+
+
+//all the web scraping will happen here
+//res.send('hello bye');
+});
+
+app.listen(port);
+console.log('magic happens on port' + port);
+
+exports = module.exports = app;
